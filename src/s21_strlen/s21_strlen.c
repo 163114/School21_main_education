5acc3a59f1b5d7af8d5f3e81539bb933ee1ba70b
Elvia Attack <elviaatt@su-i5.msk.21-school.ru>
2022-05-03 22:18:07 +0300

    develop-mike-elviaatt/Makefile works. Makefile gathers all tests in one file and then uses checkmk successfully. TODO: change dependency path for generated test file not to have build in it; make Makefile cleaner, get rid of useless code and commented code.

--- src/s21_strlen/s21_strlen.c
+++ src/s21_strlen/s21_strlen.c
@@ -1,5 +1,4 @@
 #include "s21_strlen.h"
-#include "s21_strlen1.h"
 
 s21_size_t s21_strlen(const char *string) {
     s21_size_t length = 0ul;
--- src/s21_strlen/s21_strlen.h
+++ src/s21_strlen/s21_strlen.h
@@ -1,7 +1,7 @@
 #ifndef SRC_S21_STRLEN_S21_STRLEN_H
 #define SRC_S21_STRLEN_S21_STRLEN_H
 
-typedef s21_size_t long unsigned;
+typedef long unsigned s21_size_t;
 
 s21_size_t s21_strlen(const char *string);
 
