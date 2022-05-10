29ed647ee8e73547859e79b039e37fe7b5b6f195
Elvia Attack <elviaatt@su-i6.msk.21-school.ru>
2022-05-10 16:55:48 +0300

    develop-mike-elviaatt-ready/s21_memcpy is added, tests are added, function works correctly

--- src/s21_memcpy/s21_memcpy.c
+++ src/s21_memcpy/s21_memcpy.c
@@ -1,6 +1,15 @@
 #include "s21_memcpy.h"
 
+static char *s21_memcpy_uchar(unsigned char *destination, const unsigned char*source, s21_size_t size) {
+    for (s21_size_t index = 0ul; index < size; ++index)
+        destination[index] = source[index];
+
+    return destination;
+}
+
 void *s21_memcpy(void *destination, const void *source, s21_size_t size) {
-    //  YOUR CODE IS HERE
-    return s21_NULL;
+    unsigned char *destination_uchar = (unsigned char*)destination;
+    const unsigned char *source_uchar = (const unsigned char*)source;
+
+    return s21_memcpy_uchar(destination_uchar, source_uchar, size);
 }
