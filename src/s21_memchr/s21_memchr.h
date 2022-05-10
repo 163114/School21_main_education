7dacee60a3224fe0c9d91e575e3bdcec444a6c85
Elvia Attack <elviaatt@su-i6.msk.21-school.ru>
2022-05-10 15:47:16 +0300

    develop-mike-elviaatt-ready/changed includes

--- src/s21_memchr/s21_memchr.h
+++ src/s21_memchr/s21_memchr.h
@@ -1,7 +1,7 @@
 #ifndef SRC_S21_MEMCHR_S21_MEMCHR_H
 #define SRC_S21_MEMCHR_S21_MEMCHR_H
 
-#include "../s21_string.h"
+#include "../s21_string_types.h"
 
 void *s21_memchr(const void *string, int symbol, s21_size_t size);
 
