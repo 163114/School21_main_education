d73b8c35fbd3efa2fe7dcb6265c7647cd8a591d0
Elvia Attack <elviaatt@su-i6.msk.21-school.ru>
2022-05-10 22:15:35 +0300

    develop-mike-elviaatt-ready/s21_string.h is modified to include s21_memset.h

--- src/s21_string.h
+++ src/s21_string.h
@@ -8,6 +8,9 @@
 #include "s21_memchr/s21_memchr.h"
 #include "s21_memcmp/s21_memcmp.h"
 #include "s21_memcpy/s21_memcpy.h"
+#include "s21_memmove/s21_memmove.h"
+#include "s21_memset/s21_memset.h"
+
 #include "s21_strlen/s21_strlen.h"
 #include "s21_strcmp/s21_strcmp.h"
 
