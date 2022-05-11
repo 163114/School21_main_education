12a13cc50ccc6d665f360509ce51467ed7180598
Finch Renda <finchren@student.21-school.ru>
2022-05-11 12:44:59 +0000

    Delete s21_sprintf.h

--- src/s21_sprintf/s21_sprintf.h
+++
@@ -1,8 +0,0 @@
-#ifndef SRC_S21_SPRINTF_S21_SPRINTF_H
-#define SRC_S21_SPRINTF_S21_SPRINTF_H
-
-#include "../s21_string.h"
-
-int s21_sprintf(char *buffer, const char *format, ...);
-
-#endif  //  SRC_S21_SPRINTF_S21_SPRINTF_H
