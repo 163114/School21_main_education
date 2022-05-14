81417e44e9231ce248c89b6bcb54344e2174dfb1
Finch Renda <finchren@su-i4.msk.21-school.ru>
2022-05-14 17:08:53 +0300

    vararg isn't working as planned

--- src/s21_sprintf/s21_sprintf_new.c
+++ src/s21_sprintf/s21_sprintf_new.c
@@ -36,7 +36,8 @@ int main() {
         char exclamation_point = '!';
         // int number = 15;
         s21_sprintf(buffer, "Hello world%c!%c\n", exclamation_point, exclamation_point);
-        puts(buffer);
+        // puts(buffer);
+        printf("buffer = %s\n", buffer);
 
     }
     free(buffer);
@@ -55,8 +56,8 @@ int s21_sprintf(char *buffer, const char *format, ...) {
         ++format;
         // Here we can call another function to which we can pass variable arguments by passing a single va_list pointer
         choose_return_type(buffer, format, index, argp);
-        int for_testing = va_arg(argp, int);
-        printf("inside the while loop = %c, run number = %d\n", for_testing, index);
+        // int for_testing = va_arg(argp, int);
+        // printf("inside the while loop = %c, run number = %d\n", for_testing, index);
         } else {
             buffer[index] = *format;
             buffer = (char*)realloc(buffer, (realloc_counter)*sizeof(char));
@@ -70,17 +71,19 @@ int s21_sprintf(char *buffer, const char *format, ...) {
 }
 
 void choose_return_type(char *buffer, const char *format, int index, va_list argp) {
-    int for_testing = va_arg(argp, int);
-    printf("inside the choose return type = %c, run number = %d\n", for_testing, index);
-    if (*format == 'c') {
+    // int for_testing = va_arg(argp, int);
+    // printf("inside the choose return type = %c, run number = %d\n", for_testing, index);
+    if ('c' == *format) {
         c_specifier(buffer, index, argp);
     }
     
 }
 
 void c_specifier(char *buffer, int index, va_list argp) {
-    int for_testing = va_arg(argp, int);
-    printf("inside the c specifier = %c, run number = %d\n", for_testing, index);
-    int char_to_append = va_arg(argp, int);
-    buffer[index] = (char)char_to_append;
+    // int for_testing = va_arg(argp, int);
+    // printf("inside the c specifier = %c, run number = %d\n", for_testing, index);
+    // int char_to_append = va_arg(argp, int);
+    // buffer[index] = va_arg(argp, int);
+    buffer[index] = va_arg(argp, int);
+    printf("buffer[index] = %d  %c\n", buffer[index], buffer[index]);
 }
