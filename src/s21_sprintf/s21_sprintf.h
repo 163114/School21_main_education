f627127890ad89f223d0f97d075782017f93e10c
Elvia Attack <elviaatt@su-i6.msk.21-school.ru>
2022-05-14 18:47:19 +0300

    develop-mike-elviaatt/s21_sprintf - added test examples, .gitignore is modify to ingore files produced for coverage report

--- src/s21_sprintf/s21_sprintf.h
+++ src/s21_sprintf/s21_sprintf.h
@@ -1,6 +1,7 @@
 #ifndef SRC_S21_SPRINTF_S21_SPRINTF_H
 #define SRC_S21_SPRINTF_S21_SPRINTF_H
 
+#include <stdarg.h>
 #include "../s21_string.h"
 
 int s21_sprintf(char *buffer, const char *format, ...);
