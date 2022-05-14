99f6a7ba632bca5cb14664c4d0de52b1074d2146
Finch Renda <finchren@su-i4.msk.21-school.ru>
2022-05-14 17:33:54 +0300

    Solved mistake with else case + removed dynamic memory allocation in sprintf

--- src/s21_sprintf/s21_sprintf_new.c
+++ src/s21_sprintf/s21_sprintf_new.c
@@ -27,21 +27,11 @@ Task - Implement the sprintf function from the stdio.h library
 #include "s21_sprintf_new.h"
 
 int main() {
-    char *buffer;
-    buffer = (char*)malloc(1*sizeof(char));
-    if (buffer == NULL) {
-        printf("Memory could not be allocated");
-    } else {
-
-        char exclamation_point = '!';
-        // int number = 15;
-        s21_sprintf(buffer, "Hello world%c!%c\n", exclamation_point, exclamation_point);
-        // puts(buffer);
-        printf("buffer = %s\n", buffer);
-
-    }
-    free(buffer);
-    buffer = NULL;
+    char buffer[100];
+    char exclamation_point = '!';
+    // int number = 15;
+    s21_sprintf(buffer, "Hello world%c!%c\n", exclamation_point, exclamation_point);
+    puts(buffer);
     return 0;
 }
 
@@ -50,17 +40,16 @@ int s21_sprintf(char *buffer, const char *format, ...) {
     va_list argp;
     // After calling va_start argp points at the first vararg argument
     va_start(argp, format);
-    int realloc_counter = 2, index = 0;
+    int index = 0;
     while (*format != '\0') {
         if (*format == '%') {
         ++format;
         // Here we can call another function to which we can pass variable arguments by passing a single va_list pointer
         choose_return_type(buffer, format, index, argp);
-        // int for_testing = va_arg(argp, int);
-        // printf("inside the while loop = %c, run number = %d\n", for_testing, index);
+        ++format;
+        ++index;
         } else {
             buffer[index] = *format;
-            buffer = (char*)realloc(buffer, (realloc_counter)*sizeof(char));
             ++index;
             ++format;
         }
@@ -71,8 +60,6 @@ int s21_sprintf(char *buffer, const char *format, ...) {
 }
 
 void choose_return_type(char *buffer, const char *format, int index, va_list argp) {
-    // int for_testing = va_arg(argp, int);
-    // printf("inside the choose return type = %c, run number = %d\n", for_testing, index);
     if ('c' == *format) {
         c_specifier(buffer, index, argp);
     }
@@ -80,10 +67,5 @@ void choose_return_type(char *buffer, const char *format, int index, va_list arg
 }
 
 void c_specifier(char *buffer, int index, va_list argp) {
-    // int for_testing = va_arg(argp, int);
-    // printf("inside the c specifier = %c, run number = %d\n", for_testing, index);
-    // int char_to_append = va_arg(argp, int);
-    // buffer[index] = va_arg(argp, int);
     buffer[index] = va_arg(argp, int);
-    printf("buffer[index] = %d  %c\n", buffer[index], buffer[index]);
 }
