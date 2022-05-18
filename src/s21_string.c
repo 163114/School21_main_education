94da7b1271fcd968c86e47a85c951861d4078d8e
Arryn Pallas <arrynpal@ga-q5.msk.21-school.ru>
2022-05-18 18:16:57 +0300

    a

--- src/s21_string.c
+++ src/s21_string.c
@@ -29,7 +29,7 @@ char	*s21_strchr(const char *__s, int __c)
 {
     char *p = s21_NULL;
     for (; *__s; __s++)
-        if ((int) *__s == __c) p = __s
+        if ((int) *__s == __c) p = __s;
     return p;
 }
 
