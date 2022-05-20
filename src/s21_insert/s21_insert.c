2636fb85aa673c997120e6efa12481f0e5f8b5f4
Arryn Pallas <arrynpal@ga-q5.msk.21-school.ru>
2022-05-20 19:02:05 +0300

    20 may

--- src/s21_insert/s21_insert.c
+++ src/s21_insert/s21_insert.c
@@ -3,7 +3,7 @@
 void *s21_insert(const char *src, const char *str, s21_size_t start_index)
 {
     int len1 = s21_strlen(src), len2 = s21_strlen(str), len = len1 + len2, i = 0, j = 0, k = 0;
-    char *str_ = (char*) malloc(len1 + len2 * sizeof(char));
+    char *str_ = malloc(len1 + len2 * sizeof(char));
     if (str_){
         for (; i < len; i++)
         {
--- src/s21_string.c
+++ src/s21_string.c
@@ -6,8 +6,8 @@
 int main()
 {
     char s1[] = "hi my frends", s2[] = "hi frends", s3[] = "qwertyuiop[]\';lkjhgfdsa`zxcvbnm,./1234567890-";
-    printf("%s\t%s\n", s1, s2);
-    printf("%s\n", s21_trim("....fine***", ".*"));
+    // printf("%s\t%s\n", s1, s2);
+    printf("%s\n", s21_trim(s3, ".*qwer1234567890ty-uiop"));
     return 0;
 }
 
@@ -86,47 +86,45 @@ void *s21_to_lower(const char *str)
 void *s21_insert(const char *src, const char *str, s21_size_t start_index)
 {
     int len1 = s21_strlen(src), len2 = s21_strlen(str), len = len1 + len2, i = 0, j = 0, k = 0;
-    char *str_ = (char*) malloc(len1 + len2 * sizeof(char));
-    for (; i < len; i++)
-    {
-        if (i >= start_index && k < len2)
+    char *str_ = (char *)malloc(len1 + len2 * sizeof(char));
+    if (str_){
+        for (; i < len; i++)
         {
-            str_[i] = str[k];
-            k++;
-        } else {
-            str_[i] = src[j];
-            j++;
+            if (i >= start_index && k < len2)
+            {
+                str_[i] = str[k];
+                k++;
+            } else {
+                str_[i] = src[j];
+                j++;
+            }
         }
     }
     return str_;
 }
 
-// don't work
+int searc(char c, const char *trim_chars)
+{
+    int lentr = s21_strlen(trim_chars);
+    for (int i = 0; i < lentr; i++)
+        if (c == trim_chars[i]) return 1;
+    return 0;
+}
+
 void *s21_trim(const char *src, const char *trim_chars)
 {
-    int len1 = s21_strlen(src), len2 = s21_strlen(trim_chars), start_n = 1, start = 0, end_n = 1, end = 0;
-    for (int i = 0; i < len1; i++)
+    int len = s21_strlen(src), begin = 0, end = len - 1, lenst = len;
+
+    for (; begin < len; begin++)
+        if (!searc(src[begin], trim_chars)) break;
+    for (; end > begin; end--)
+        if (!searc(src[end], trim_chars)) break;
+    lenst = len - begin - (len - end) + 1;
+    char *str_ = (char *)malloc(lenst * sizeof(char));
+    if (str_)
     {
-        int t = 0;
-        char c = src[i];
-        if (start_n)
-            for (int j = 0; j < len2; j++)
-                if (c == trim_chars[j]) t = 1;
-        if (!t) break;
-        else start++;
+        for (int i = 0; i < lenst && begin <= end; i++, begin++)
+            str_[i] = src[begin];
     }
-    for (int i = len1 - 1; i > -1; i++)
-    {
-        int t = 0;
-        char c = src[i];
-        if (end_n)
-            for (int j = 0; j < len2; j++)
-                if (c == trim_chars[j]) t = 1;
-        if (!t) end_n = 0;
-        else end++;
-    }
-    char *str_ = (char*) malloc(len1 - start - end + sizeof(char));
-    for (int i = 0; i < s21_strlen(str_); i++)
-        if (i >= start && i <= end) str_[i - start] = src[i];
     return str_;
 }
