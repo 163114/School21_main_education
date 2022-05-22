d4ec87e505d9975cc0571e09eaa9785a933f8200
Elvia Attack <elviaatt@su-i6.msk.21-school.ru>
2022-05-22 18:12:28 +0300

    develop-mike-elviaatt-ready/s21_strtok - is ready, tests are ready

--- src/s21_strtok/s21_strtok.c
+++ src/s21_strtok/s21_strtok.c
@@ -2,8 +2,8 @@
 #include "../s21_common/s21_is_found/s21_is_found.h"
 #include "../s21_strlen/s21_strlen.h"
 
-static int is_delimiter(const unsigned char* delimiters, unsigned char symbol, s21_size_t length) {
-    return is_found(delimiters, symbol, length);
+static int __is_delimiter(const char* delimiters, char symbol, s21_size_t length) {
+    return is_found((const unsigned char *)delimiters, symbol, length);
 }
 
 char *s21_strtok(char *string, const char *delimiters) {
@@ -12,7 +12,6 @@ char *s21_strtok(char *string, const char *delimiters) {
     static s21_size_t index = 0ul;
 
     if (string) {
-        // printf("new string\n");
         string_to_remember = string;
         length_string = s21_strlen(string);
         index = 0ul;
@@ -22,42 +21,37 @@ char *s21_strtok(char *string, const char *delimiters) {
     s21_size_t length_token = 0ul;
     s21_size_t start_index = index;
 
-//     012345678901234
-//    "URGH DA FUCK!"
     int is_previous_delimiter = 1;
     char *result_token = s21_NULL;
     int go_on = 1;
     while (index < length_string && go_on) {
-        if (is_delimiter(delimiters, string_to_remember[index], length_delimiters)) {
+        if (__is_delimiter(delimiters, string_to_remember[index], length_delimiters)) {
             
             if (!is_previous_delimiter)
                 string_to_remember[index] = '\0';
             is_previous_delimiter = 1;
-            ++index; //
-
             
             if (length_token > 0ul) {
-                const s21_size_t old_start_index = start_index;
-                start_index = index - 1;
-                
-                //printf("index-1 = %zu\told_start_index = %zu\tlength_token = %zu\n", index-1, old_start_index, length_token);
-                length_token = 0ul;
-                result_token = string_to_remember + old_start_index;
+                result_token = string_to_remember + start_index;
+
+                start_index = index;
                 go_on = 0;
+                length_token = 0ul;
             }
         } else {
-            ++index; // 
             ++length_token;
+
             if (is_previous_delimiter)
-                start_index = index - 1;
+                start_index = index;
+
             is_previous_delimiter = 0;
         }
+
+        ++index;
     }
 
-    if (length_token > 0ul) {
-        //printf("index-1 = %zu\tstart_index\t= %zu\tlength_token = %zu\n", index-1, start_index, length_token);
+    if (length_token > 0ul)
         result_token = string_to_remember + start_index;
-    }
 
     return result_token;
 }
