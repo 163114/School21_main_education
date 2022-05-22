5b99ce1ee6d11592997252d96223e342968ce91b
Finch Renda <finchren@su-i4.msk.21-school.ru>
2022-05-22 15:19:05 +0300

    Trying to complete %f

--- src/s21_sprintf/test.c
+++ src/s21_sprintf/test.c
@@ -3,19 +3,20 @@
 #include <math.h>
 
 int main() {
-    // // -3402823466385288598117041834845169254400.0000000000000000 Float lowest
+    // // -3402823466385288598117041834845169254400.000000 Float lowest
     // //340282346638528859811704183484516925440.000000 Float max
     // double lowest = -340282346638528859811704183484516925440.000000;
     // // float max = 340282346638528859811704183484516925440.000000;
     // char first_array[1024];
-    // // char second_array[58];
+    char second_array[58];
     // sprintf(first_array, "%f", lowest);
     // // sprintf(second_array, "%f", max);
     // puts(first_array);
     // // puts(second_array);
     // return 0;
-    double number = 12345.12345;
-    double power_of_ten = pow(10, -5);
-    double result = number / power_of_ten;
-    printf("%f", result);
+    double number = 1;
+    // double power_of_ten = pow(10, -5);
+    // double result = number / power_of_ten;
+    sprintf(second_array, "%f", number);
+    puts(second_array);
 }
