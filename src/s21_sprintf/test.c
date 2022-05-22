b02e054ac54d5b13bb185bf7edbe2b903d03c516
Finch Renda <finchren@su-i4.msk.21-school.ru>
2022-05-22 16:47:43 +0300

    Almost finished with %f. Need to fix the bugs

--- src/s21_sprintf/test.c
+++ src/s21_sprintf/test.c
@@ -6,7 +6,7 @@ int main() {
     // // -3402823466385288598117041834845169254400.000000 Float lowest
     // //340282346638528859811704183484516925440.000000 Float max
     // double lowest = -340282346638528859811704183484516925440.000000;
-    // // float max = 340282346638528859811704183484516925440.000000;
+    double max = 340282346638528859811704183484516925440.000000;
     // char first_array[1024];
     char second_array[58];
     // sprintf(first_array, "%f", lowest);
@@ -14,9 +14,9 @@ int main() {
     // puts(first_array);
     // // puts(second_array);
     // return 0;
-    double number = 1;
+    // double number = 1;
     // double power_of_ten = pow(10, -5);
     // double result = number / power_of_ten;
-    sprintf(second_array, "%f", number);
+    sprintf(second_array, "%f", max);
     puts(second_array);
 }
